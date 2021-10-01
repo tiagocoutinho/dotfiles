a84ea1fe0cd5e56b9197f0e96d7e961b313c1ee9
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-10-01 18:38:06 +0200

    Remove fish conda and asdf (they are not configured yet)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,3 @@
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/tcoutinho/miniconda/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
-source ~/.asdf/asdf.fish
-
 function fish_greeting
 #    echo Logged in to (set_color blue;echo $hostname;set_color normal) at (set_color yellow;date +%T;set_color normal) 
 end
